6859f004b7f6bfc98d224c1d01ef2e26ac08d111
l1terally <literallyuriy@gmail.com>
2023-01-04 22:50:46 +0800

    Initial Commit

---
+++ menush/menushApp.swift
@@ -0,0 +1,17 @@
+//
+//  menushApp.swift
+//  menush
+//
+//  Created by Юрий  Лизунов on 04/01/23.
+//
+
+import SwiftUI
+
+@main
+struct menushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
