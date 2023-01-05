dc619bab44de23e39ae5489c489886aa346e78a7
l1terally <literallyuriy@gmail.com>
2023-01-05 20:33:13 +0800

    Done with it

--- menush/ContentView.swift
+++ menush/ContentView.swift
@@ -1,21 +1,267 @@
-//
-//  ContentView.swift
-//  menush
-//
-//  Created by Юрий  Лизунов on 04/01/23.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var index = 0
+    @State var show = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        
+        ZStack{
+            
+            HStack{
+                VStack(alignment: .leading, spacing: 12) {
+                    Image("avatar")
+                        .clipShape(Circle())
+                    Text("Hello,")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.top, 10)
+                    Text("User")
+                        .fontWeight(.bold)
+                        .font(.title)
+                        .foregroundColor(.white)
+                    Button(action: {
+                        self.index = 0
+                        
+                        withAnimation{
+                            self.show.toggle()
+                        }
+                    }) {
+                        HStack(spacing: 25){
+                            Image(systemName: "envelope.open.fill")
+                                .foregroundColor(self.index == 0 ? Color("Color1") : Color.white)
+                            
+                            Text("Catalogue")
+                                .foregroundColor(self.index == 0 ? Color("Color1") : Color.white)
+                        }
+                        .padding(.vertical, 10)
+                        .padding(.horizontal)
+                        .background(self.index == 0 ? Color("Color1").opacity(0.2) : Color.clear)
+                        .cornerRadius(10)
+                    }
+                    .padding(.top,25)
+                    
+                    Button(action: {
+                        self.index = 1
+                        
+                        withAnimation{
+                            self.show.toggle()
+                        }
+                        
+                    }) {
+                        
+                        HStack(spacing: 25){
+                            
+                            Image(systemName: "cart.fill")
+                                .foregroundColor(self.index == 1 ? Color("Color1") : Color.white)
+                            
+                            
+                            Text("Cart")
+                                .foregroundColor(self.index == 1 ? Color("Color1") : Color.white)
+                        }
+                        .padding(.vertical, 10)
+                        .padding(.horizontal)
+                        .background(self.index == 1 ? Color("Color1").opacity(0.2) : Color.clear)
+                        .cornerRadius(10)
+                    }
+                    
+                    Button(action: {
+                        
+                        self.index = 2
+                        
+                        withAnimation{
+                            
+                            self.show.toggle()
+                        }
+                        
+                    }) {
+                        
+                        HStack(spacing: 25){
+                            
+                            Image(systemName: "heart.fill")
+                                .foregroundColor(self.index == 2 ? Color("Color1") : Color.white)
+                            
+                            
+                            Text("Favorite")
+                                .foregroundColor(self.index == 2 ? Color("Color1") : Color.white)
+                        }
+                        .padding(.vertical, 10)
+                        .padding(.horizontal)
+                        .background(self.index == 2 ? Color("Color1").opacity(0.2) : Color.clear)
+                        .cornerRadius(10)
+                    }
+                    
+                    Button(action: {
+                        
+                        self.index = 3
+                        
+                        withAnimation{
+                            
+                            self.show.toggle()
+                        }
+                        
+                    }) {
+                        
+                        HStack(spacing: 25){
+                            
+                            Image(systemName: "airport.express")
+                                .foregroundColor(self.index == 3 ? Color("Color1") : Color.white)
+                            
+                            
+                            Text("My orders")
+                                .foregroundColor(self.index == 3 ? Color("Color1") : Color.white)
+                        }
+                        .padding(.vertical, 10)
+                        .padding(.horizontal)
+                        .background(self.index == 3 ? Color("Color1").opacity(0.2) : Color.clear)
+                        .cornerRadius(10)
+                    }
+                    
+                    Divider()
+                        .frame(width: 150, height: 1)
+                        .background(Color.white)
+                        .padding(.vertical,30)
+                    
+                    Button(action: {
+                        //
+                    }) {
+                        
+                        HStack(spacing: 25){
+                            
+                            Image(systemName: "door.right.hand.open")
+                                .foregroundColor(Color.white)
+                            
+                            
+                            Text("Exit")
+                                .foregroundColor(Color.white)
+                        }
+                        .padding(.vertical, 10)
+                        .padding(.horizontal)
+                    }
+                    
+                    Spacer(minLength: 0)
+                }
+                .padding(.top,25)
+                .padding(.horizontal,20)
+                
+                Spacer(minLength: 0)
+            }
+            .padding(.top,UIApplication.shared.windows.first?.safeAreaInsets.top)
+            .padding(.bottom,UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+            
+            
+            VStack(spacing: 0){
+                
+                HStack(spacing: 15){
+                    
+                    Button(action: {
+                        
+                        withAnimation{
+                            
+                            self.show.toggle()
+                        }
+                        
+                    }) {
+                        
+                        
+                        Image(systemName: self.show ? "xmark" : "line.horizontal.3")
+                            .resizable()
+                            .frame(width: self.show ? 18 : 22, height: 18)
+                            .foregroundColor(Color.black.opacity(0.4))
+                    }
+                    
+                    
+                    Text(self.index == 0 ? "Catalogue" : (self.index == 1 ? "Cart" : (self.index == 2 ? "Favorite" : "My orders")))
+                        .font(.title)
+                        .foregroundColor(Color.black.opacity(0.6))
+                    
+                    Spacer(minLength: 0)
+                }
+                .padding(.top,UIApplication.shared.windows.first?.safeAreaInsets.top)
+                .padding()
+                
+                GeometryReader{_ in
+                    
+                    VStack{
+                        
+                        if self.index == 0{
+                            
+                            Catalogue()
+                        }
+                        else if self.index == 1{
+                            
+                            Cart()
+                        }
+                        else if self.index == 2{
+                            
+                            Fav()
+                        }
+                        else{
+                            
+                            Orders()
+                        }
+                    }
+                }
+            }
+            .background(Color.white)
+            .cornerRadius(self.show ? 30 : 0)
+            .scaleEffect(self.show ? 0.9 : 1)
+            .offset(x: self.show ? UIScreen.main.bounds.width / 2 : 0, y: self.show ? 15 : 0)
+            .rotationEffect(.init(degrees: self.show ? -5 : 0))
+            .edgesIgnoringSafeArea(.all)
         }
-        .padding()
+        .background(Color("Color").edgesIgnoringSafeArea(.all))
+    }
+}
+
+struct Catalogue : View {
+    
+    var body: some View{
+        
+        VStack{
+            
+            Text("Catalogue")
+        }
+        
+    }
+    
+}
+
+
+struct Cart : View {
+    
+    var body: some View{
+        
+        VStack{
+            
+            Text("Cart")
+        }
+    }
+}
+
+struct Orders : View {
+    
+    var body: some View{
+        
+        VStack{
+            
+            Text("My orders")
+        }
+    }
+}
+
+struct Fav : View {
+    
+    var body: some View{
+        
+        VStack{
+            
+            Text("Favorite")
+        }
     }
 }
 
@@ -24,3 +270,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
